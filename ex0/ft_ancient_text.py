32eb97a52139415285ea24ec62015a7289dd1ec4
gmach <gmach@student.42lyon.fr>
2026-01-06 10:37:53 +0100

    ex0

--- ex0/ft_ancient_text.py
+++ ex0/ft_ancient_text.py
@@ -1,1 +1,44 @@
-import sys
+#!/usr/bin/env python3
+
+
+def access_vault(file):
+    """function to open a file"""
+    print(f"Accessing Storage Vault: {file}")
+    try:
+        f = open(file, "r")
+        print("Connection established...\n")
+        return (f)
+    except FileNotFoundError:
+        print("ERROR: Storage vault not found.")
+        return None
+
+
+def read_vault(file):
+    """function to read a file"""
+    print("RECOVERED DATA:")
+    try:
+        print(file.read())
+        print("\n")
+    except AttributeError as e:
+        print(f"Error: {e}")
+
+
+def close_vault(file):
+    """function to close a file"""
+    try:
+        file.close()
+        print("Data recovery complete. Storage unit disconnected.\n")
+    except AttributeError as e:
+        print(f"Error: {e}")
+
+
+def main():
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+    file = access_vault("ancient_fragment.txt")
+    if file is not None:
+        read_vault(file)
+        close_vault(file)
+
+
+if __name__ == "__main__":
+    main()
