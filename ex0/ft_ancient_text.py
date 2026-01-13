c398b420df67d1a0d6dfacee3f46b10a2fec2397
gmach <gmach@student.42lyon.fr>
2026-01-13 16:22:59 +0100

    save ex0

--- ex0/ft_ancient_text.py
+++ ex0/ft_ancient_text.py
@@ -1,43 +1,58 @@
 #!/usr/bin/env python3
 
 
-def access_vault(file):
-    """function to open a file"""
-    print(f"Accessing Storage Vault: {file}")
-    try:
-        f = open(file, "r")
-        print("Connection established...\n")
-        return (f)
-    except FileNotFoundError:
-        print("ERROR: Storage vault not found.")
-        return None
+# def access_vault(file):
+#     """function to open a file"""
+#     print(f"Accessing Storage Vault: {file}")
+#     try:
+#         f = open(file, "r")
+#         print("Connection established...\n")
+#         return (f)
+#     except FileNotFoundError:
+#         print("ERROR: Storage vault not found.")
+#         return None
 
 
-def read_vault(file):
-    """function to read a file"""
-    print("RECOVERED DATA:")
-    try:
-        print(file.read())
-        print("\n")
-    except AttributeError as e:
-        print(f"Error: {e}")
+# def read_vault(file):
+#     """function to read a file"""
+#     print("RECOVERED DATA:")
+#     try:
+#         print(file.read())
+#         print("\n")
+#     except AttributeError as e:
+#         print(f"Error: {e}")
 
 
-def close_vault(file):
-    """function to close a file"""
-    try:
-        file.close()
-        print("Data recovery complete. Storage unit disconnected.\n")
-    except AttributeError as e:
-        print(f"Error: {e}")
+# def close_vault(file):
+#     """function to close a file"""
+#     try:
+#         file.close()
+#         print("Data recovery complete. Storage unit disconnected.\n")
+#     except AttributeError as e:
+#         print(f"Error: {e}")
 
+
+# def main():
+#     print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+#     file = access_vault("ancient_fragment.txt")
+#     if file is not None:
+#         read_vault(file)
+#         close_vault(file)
 
 def main():
-    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
-    file = access_vault("ancient_fragment.txt")
-    if file is not None:
-        read_vault(file)
-        close_vault(file)
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===")
+    file_name = "ancient_fragment.txt"
+    try:
+        print(f"\nAccessing Storage Vault: {file_name}")
+        file = open(file_name, 'r')
+        print("Connection established...")
+    except Exception as e:
+        print(f"Error: {e}")
+    else:
+        print("\nRECOVERED DATA:")
+        print(file.read())
+        file.close()
+        print("\nData recovery complete. Storage unit disconnected.")
 
 
 if __name__ == "__main__":
